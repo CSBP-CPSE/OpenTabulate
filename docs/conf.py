755178fab3d1a64a7af30abf16e1ab76ade3043f
Maksym Neyra-Nesterenko <37066707+VirtualTorus@users.noreply.github.com>
2020-01-03 11:27:52 -0800

    doc config change
    Changed to overcome ReadTheDocs build issue.

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,10 @@ release = '2.0.0'
 
 # -- General configuration ---------------------------------------------------
 
+# The document name of the “master” document, that is, the document that
+# contains the root toctree directive. 
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
